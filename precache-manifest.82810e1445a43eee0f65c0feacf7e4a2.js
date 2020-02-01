5d7cacc51c455379b31b9bc4b01a61143e0f04fb
samtir <samtirmar06@gmail.com>
2020-01-31 21:07:01 -0600

    Add files via upload
    Se agrego

---
+++ precache-manifest.82810e1445a43eee0f65c0feacf7e4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cdabafc355dd2952fa",
+    "url": "css/app.71831ee0.css"
+  },
+  {
+    "revision": "28806237c937fc2e084743fe036ed651",
+    "url": "index.html"
+  },
+  {
+    "revision": "36cdabafc355dd2952fa",
+    "url": "js/app.059fa75e.js"
+  },
+  {
+    "revision": "eb59edce0d730c39c242",
+    "url": "js/chunk-vendors.a57d1c7d.js"
+  },
+  {
+    "revision": "06f8bf480ceafaa7544d2d7bbb20c1ec",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
